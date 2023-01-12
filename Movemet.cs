aab5527ebb2d50c9f9a2bd841843b4fbeb440026
vishwajeetvissus <31235409+vishwajeetvissus@users.noreply.github.com>
2023-01-12 12:20:35 +0530

    Create Movemet.cs

---
+++ Movemet.cs
@@ -0,0 +1,61 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+  [SerializeField] float mainThrust= 1000f;
+  [SerializeField] float rotationThrust= 100f;
+  [SerializeField] AudioClip mainEngine;
+  
+   Rigidbody rb;
+   AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb= GetComponent<Rigidbody>();
+        audioSource= GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+    void ProcessThrust()
+    {
+       if(Input.GetKey(KeyCode.Space))
+       {
+         rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+         if(!audioSource.isPlaying)
+         {
+           audioSource.PlayOneShot(mainEngine);
+         }
+        }
+       else
+       {
+         audioSource.Stop();
+       }
+    }
+       void ProcessRotation() 
+       {
+          if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if(Input.GetKey(KeyCode.D))
+          {
+            ApplyRotation(-rotationThrust);
+          }
+        }
+
+        void ApplyRotation(float rotationThisFframe)
+    {
+        rb.freezeRotation= true;
+        transform.Rotate(Vector3.forward * rotationThisFframe * Time.deltaTime);
+        rb.freezeRotation= false;
+    }
+
+}
+
